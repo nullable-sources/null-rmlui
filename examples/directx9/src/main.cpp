5b39e9293434feec8d7a6d7c7ac8e86355397f24
suddenly <suddenlybydl0@gmail.com>
2023-02-14 02:03:02 +0500

    Updated null-render.

--- examples/directx9/src/main.cpp
+++ examples/directx9/src/main.cpp
@@ -7,7 +7,7 @@ utils::c_cumulative_time_measurement frame_counter{ 60 };
 
 void main_loop() {
 	null::render::begin_frame(window); {
-		null::render::background.draw_text(std::format("[ directx9 ] fps: {:3.0f}", 1.f / std::chrono::duration<float>{ frame_counter.representation() }.count()), { window.get_window_size().x, 10.f }, { }, null::render::e_text_flags{ -null::render::e_text_flags::aligin_right | -null::render::e_text_flags::aligin_center_y | -null::render::e_text_flags::outline });
+		null::render::background.add_text(std::format("[ directx9 ] fps: {:3.0f}", 1.f / std::chrono::duration<float>{ frame_counter.representation() }.count()), { window.get_window_size().x, 10.f }, { }, null::render::e_text_flags{ -null::render::e_text_flags::aligin_right | -null::render::e_text_flags::aligin_center_y | -null::render::e_text_flags::outline });
 
 		context->Update();
 		context->Render();
